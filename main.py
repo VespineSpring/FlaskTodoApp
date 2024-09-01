1a59d5c2d23b31b8d72411e9f0e7f45a0a99764b
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-09-01 12:28:23 +0530

    Actually started working on Todo app

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 from flask import Flask, redirect, url_for, request, render_template
+import json
+import os
 
 
 app = Flask(__name__)
+task_file = "tasks.json"
 
 
-@app.route("/")
+if not os.path.exists(task_file):
+   with open(task_file, "w") as file:
+        json.dump([], file)
+
+
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+   tasks = get_tasks()
+
+   if request.method == "POST":
+      task = request.form["task"]
+
+      with open(task_file, "r") as file:
+         data = json.load(file)
+
+      data.append(task)
+
+      with open(task_file, "w") as file:
+         json.dump(data, file, indent=4)
+
+      return redirect(url_for("index"))
+   else:
+      return render_template("index.html", tasks=tasks)
+   
+
+def get_tasks():
+   with open(task_file, "r") as file:
+      tasks = json.load(file)
+
+   return tasks
 
 
 @app.route("/user/<username>")
