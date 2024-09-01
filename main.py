de1e6034e1dc4a26523e87a41e058a9fbff266bb
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-09-01 07:48:36 +0530

    Learnt few things.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 
 app = Flask(__name__)
@@ -9,7 +9,7 @@ def index():
     return "Hello, World!"
 
 
-@app.route("/vespine")
+@app.route("/vespine/")
 def about_vespine():
     info = """
     Hello, my name is Vespine or in short you can call me Ve.
@@ -17,6 +17,29 @@ def about_vespine():
     I have been coding since 4 years.
     """
     return info
+
+
+@app.route("/home")
+def home_page():
+    return "<h1>This is a home page.</h1>"
+
+
+@app.route("/guest/<guest>")
+def hello_guest(guest):
+   return f"Hello {guest} as Guest"
+
+
+@app.route("/admin")
+def hello_admin():
+   return 'L Admin'
+
+
+@app.route("/user/<name>")
+def hello_user(name):
+   if name =="admin":
+      return redirect(url_for("hello_admin"))
+   else:
+      return redirect(url_for("hello_guest", guest = name))
 
 
 if __name__ == "__main__":
